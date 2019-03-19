28d649f9b92a7681eb62479edc8b4f8cd5826bbc
lisakost <pupirka23@gmail.com>
2019-03-19 13:30:18 +0200

    Article template was created. Get_short_text method was created in model.

--- gis/models.py
+++ gis/models.py
@@ -4,11 +4,21 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+SHORT_TEXT_LEN = 700
+
+
 # Create your models here.
 class Articles(models.Model):
     title=models.CharField(max_length=200)
     text=models.TextField()
     user=models.ForeignKey(User)
+
+    def get_short_text(self):
+        if len(self.text) > SHORT_TEXT_LEN:
+            return self.text[:SHORT_TEXT_LEN]
+        else:
+            return self.text
+
 
     def __str__(self):
         return self.title
--- gis/views.py
+++ gis/views.py
@@ -2,8 +2,7 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from gis.models import Articles
@@ -18,6 +17,10 @@ def home(request):
         'articles': articles
     }
     return render(request, 'gis/home.html', context)
+
+def show_articles(request, article_id):
+    article = get_object_or_404(Articles, id=article_id)
+    return render(request, 'gis/article.html', {'article': article})
 
 
 def about(request):
